30cd0c04c96afb4ceef3b0f24f47c361fe671a94
Kenneth Hansen <kenneth@khansen-it.dk>
2017-12-08 18:31:44 +0100

    Add vuex store modules

--- assets/src/js/vuex/store.js
+++ assets/src/js/vuex/store.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';const LOGIN = "LOGIN";
+import state from './state';
+import getters from './getters';
+import mutations from './mutations';
+import actions from './actions';
 
 const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 const LOGIN_FAILED = "LOGIN_FAILED";
 const LOGOUT = "LOGOUT";
 
 export const store = new Vuex.Store({
-    
+    state,
+    getters,
+    mutations,
+    actions
 });
