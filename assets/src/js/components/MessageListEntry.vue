b65a646ec154ac79f02277c48d827710e72aa28c
Kenneth Hansen <kenneth@khansen-it.dk>
2018-01-20 11:21:55 +0100

    Fix broken click event after component extraction

--- assets/src/js/components/MessageListEntry.vue
+++ assets/src/js/components/MessageListEntry.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex hover:bg-grey-lighter text-sm cursor-pointer break-words">
+    <div class="flex hover:bg-grey-lighter text-sm cursor-pointer break-words" @click="$emit('click')">
         <div class="text-grey-darker w-1/3 sm:w-1/5 border-r p-2">
             <div class="">
                 <p>{{ item.from_address }}</p>
