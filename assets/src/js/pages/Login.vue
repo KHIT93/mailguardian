6c9322382b7b6faf7634ef386556fe730335d1a1
Kenneth Hansen <kenneth@khansen-it.dk>
2018-01-20 20:15:10 +0100

    Make sure that we bind the input fields

--- assets/src/js/pages/Login.vue
+++ assets/src/js/pages/Login.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container justify-center flex pt-8">
+    <div class="justify-center flex pt-8">
         <div class="w-full max-w-sm">
-            <form class="bg-white shadow-md sm:rounded px-8 pt-6 pb-8 mb-4" action="/api-auth/login/?next=/" method="POST">
+            <form @submit="submit" class="bg-white shadow-md sm:rounded px-8 pt-6 pb-8 mb-4" action="/api-auth/login/?next=/" method="POST">
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
                     <label class="block text-grey-darker font-bold md:text-right mb-1 md:mb-0 pr-4" for="username">
@@ -9,7 +9,7 @@
                     </label>
                     </div>
                     <div class="md:w-2/3">
-                        <input class="bg-grey-lighter appearance-none border border-grey-lighter hover:border-blue rounded w-full py-2 px-4 text-grey-darker" name="username" id="username" type="text" placeholder="JaneDoe@example.com">
+                        <input v-model="form.username" class="bg-grey-lighter appearance-none border border-grey-lighter hover:border-blue rounded w-full py-2 px-4 text-grey-darker" name="username" id="username" type="text" placeholder="JaneDoe@example.com">
                     </div>
                 </div>
                 <div class="md:flex md:items-center mb-6">
@@ -19,7 +19,7 @@
                     </label>
                     </div>
                     <div class="md:w-2/3">
-                        <input class="bg-grey-lighter appearance-none border border-grey-lighter hover:border-blue rounded w-full py-2 px-4 text-grey-darker" name="password" id="password" type="password" placeholder="******************">
+                        <input v-model="form.password" class="bg-grey-lighter appearance-none border border-grey-lighter hover:border-blue rounded w-full py-2 px-4 text-grey-darker" name="password" id="password" type="password" placeholder="******************">
                     </div>
                 </div>
                 <div class="flex flex-row-reverse items-center justify-between">
@@ -63,6 +63,11 @@
         },
         computed: {
             ...mapGetters(['isLoggedIn', 'csrftoken'])
+        },
+        methods: {
+            submit() {
+                this.form.post('/rest-auth/login/');
+            }
         }
     }
 </script>
