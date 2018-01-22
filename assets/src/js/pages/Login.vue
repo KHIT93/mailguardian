f92fe74fd702f4b27c37c02df93792b075bc3513
Kenneth Hansen <kenneth@khansen-it.dk>
2018-01-22 20:07:57 +0100

    Add login animation

--- assets/src/js/pages/Login.vue
+++ assets/src/js/pages/Login.vue
@@ -24,8 +24,13 @@
                 </div>
                 <div class="flex flex-row-reverse items-center justify-between">
                     <input type='hidden' name='csrfmiddlewaretoken' :value="csrftoken"/>
-                    <button class="bg-blue hover:bg-blue-dark text-white py-2 px-4 rounded" type="submit">
-                        <svg class="fill-current w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <button class="bg-blue hover:bg-blue-dark text-white py-2 px-4 rounded" type="submit" :disabled="loading" :class="{'select-none cursor-not-allowed bg-blue-lightest hover:bg-blue-lightest' : loading}">
+                        <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" v-if="loading">
+                            <circle cx="50" cy="50" fill="none" stroke-linecap="round" r="40" stroke-width="4" stroke="#fff" stroke-dasharray="62.83185307179586 62.83185307179586" transform="rotate(66 50 50)">
+                                <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform>
+                            </circle>
+                        </svg>
+                        <svg class="fill-current w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" v-else>
                             <path d="M0 0h24v24H0z" fill="none"/>
                             <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
                         </svg>
@@ -53,7 +58,8 @@
                 form: new Form({
                     username: '',
                     password: ''
-                })
+                }),
+                loading: false
             }
         },
         mounted() {
@@ -66,7 +72,7 @@
         },
         methods: {
             submit() {
-                this.form.post('/rest-auth/login/');
+                this.loading = true;
             }
         }
     }
