05475c12fe066442a0cc1bbe57a8644348c1f2b3
Kenneth Hansen <kenneth@khansen-it.dk>
2018-01-27 15:06:44 +0100

    Add tools page

---
+++ assets/src/js/pages/Tools/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div v-if="user.is_staff">
+            <h2>Admin tools</h2>
+            <ul>
+                <li>View mail queue</li>
+                <li>Clear mail queue</li>
+                <li>View job queue</li>
+                <li>Clam AV Status</li>
+                <li>GeoIP update status</li>
+                <li>SpamAssassin rules status</li>
+            </ul>
+        </div>
+        <div>
+            <h2>Helpful links</h2>
+            <ul>
+                <li><a href="http://mxtoolbox.com/NetworkTools.aspx" target="new">MXToolBox</a></li>
+                <li><a href="https://www.tcpiputils.com/" target="new">TCP IP Utilities</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    data: () => {
+        return {
+
+        }
+    },
+    computed: {
+        ...mapGetters(['isLoggedIn', 'user', 'loading'])
+    }
+}
+</script>
--- assets/src/js/routing/routes.js
+++ assets/src/js/routing/routes.js
@@ -12,6 +12,7 @@ import ReportMessageList from '../pages/Reports/Messages.vue';
 import ReportMessageOperations from '../pages/Reports/MessageOperations.vue';
 import ReportMessagesByDate from '../pages/Reports/MessagesByDate.vue';
 import ReportMessageRelays from '../pages/Reports/MessageRelays.vue';
+import Tools from '../pages/Tools/Index.vue';
 
 export default [
     { path: '/', component: Home, name: 'home' },
@@ -23,7 +24,8 @@ export default [
     { path: '/reports/messages', component: ReportMessageList, name: 'reports.messages' },
     { path: '/reports/message-operations', component: ReportMessageOperations, name: 'reports.messages.operations' },
     { path: '/reports/messages-by-date', component: ReportMessagesByDate, name: 'reports.messages.date' },
-    { path: '/reports/top-mail-relays', component: ReportMessageRelays, name: 'reports.messages.relays' },    
+    { path: '/reports/top-mail-relays', component: ReportMessageRelays, name: 'reports.messages.relays' },
+    { path: '/tools', component: Tools, name: 'tools' },
 
     /** Catchall route to display 404 page */
     { path: '*', component: NotFound, name: 'not_found' }
