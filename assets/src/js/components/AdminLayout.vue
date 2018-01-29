c8d329b9398f87310c3f2ed774ac858041f94c75
Kenneth Hansen <kenneth@khansen-it.dk>
2018-01-29 21:48:41 +0100

    Modified layout component to check if an administrator is viewing the component

--- assets/src/js/components/AdminLayout.vue
+++ assets/src/js/components/AdminLayout.vue
@@ -1,22 +1,29 @@
 <template>
-    <div class="lg:flex">
-        <div class="lg:w-1/4">
-            <mw-admin-menu></mw-admin-menu>
-        </div>
-        <div class="lg:w-3/4">
-            <div class="container mx-auto sm:px-2 pt-2 pb-8">
-                <div class="bg-white border sm:rounded shadow p-2">
-                    <slot></slot>
+    <div>
+        <div class="lg:flex" v-if="user.is_staff">
+            <div class="lg:w-1/4">
+                <mw-admin-menu></mw-admin-menu>
+            </div>
+            <div class="lg:w-3/4">
+                <div class="container mx-auto sm:px-2 pt-2 pb-8">
+                    <div class="bg-white border sm:rounded shadow p-2">
+                        <slot></slot>
+                    </div>
                 </div>
             </div>
         </div>
+        <div v-else>You are not authorized to view this page</div>
     </div>
 </template>
 <script>
 import AdminNavigation from './AdminNavigation.vue';
+import { mapGetters } from 'vuex';
 export default {
     components: {
         'mw-admin-menu': AdminNavigation
     },
+    computed: {
+        ...mapGetters(['user'])
+    }
 }
 </script>
