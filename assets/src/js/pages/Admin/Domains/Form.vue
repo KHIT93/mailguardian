fb5ec622e0265fb726c7701a6dc4c79b78f3eff4
Kenneth Hansen <kenneth@khansen-it.dk>
2018-04-05 21:14:10 +0200

    Add secure smtp as an option for relay_type

--- assets/src/js/pages/Admin/Domains/Form.vue
+++ assets/src/js/pages/Admin/Domains/Form.vue
@@ -32,6 +32,7 @@
                         <select v-model="form.relay_type" class="block appearance-none w-full bg-grey-lighter hover:border-blue border border-grey-lighter text-grey-darker py-2 px-4">
                             <option value="">Select relay type</option>
                             <option value="smtp">Deliver to my email server (SMTP)</option>
+                            <option value="smtps">Deliver to my email server (SMTP with SSL/TLS)</option>
                         </select>
                         <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
