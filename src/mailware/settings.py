d0f1deeca5d96e4ad207522968800f8be3cd7039
Kenneth Hansen <kenneth@khansen-it.dk>
2018-04-16 20:00:09 +0200

    make sure to declare static root when not in debug mode

--- src/mailware/settings.py
+++ src/mailware/settings.py
@@ -155,12 +155,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/assets/'
+ASSETS_DIR = os.path.join(os.path.dirname(BASE_DIR), "assets")
 
 if DEBUG:
-    ASSETS_DIR = os.path.join(os.path.dirname(BASE_DIR), "assets")
     STATICFILES_DIRS = [
         os.path.join(ASSETS_DIR, "dist")
     ]
+else:
+    STATIC_ROOT = os.path.join(ASSETS_DIR, "dist")
 
 
 # REST Framework configuration
