7bce64d27966e741f185aa9c46768352fd568536
Kenneth Hansen <kenneth@khansen-it.dk>
2018-04-17 19:35:24 +0200

    Remove vuex-flash, as we do not need it

--- assets/src/js/vuex/store.js
+++ assets/src/js/vuex/store.js
@@ -1,22 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexFlash from 'vuex-flash';
-import { createFlashStore } from 'vuex-flash';
 import state from './state';
 import getters from './getters';
 import mutations from './mutations';
 import actions from './actions';
 
 Vue.use(Vuex);
-Vue.use(VuexFlash, { mixin: true });
 
 
 export const store = new Vuex.Store({
     state,
     getters,
     mutations,
-    actions,
-    plugins: [
-        createFlashStore()
-    ]
+    actions
 });
