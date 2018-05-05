ff80057b7a57950dece52b1dc6ddb6cf2a4cc611
Kenneth Hansen <kenneth@khansen-it.dk>
2018-05-05 23:06:36 +0200

    Use the class instead of self

--- src/mail/models.py
+++ src/mail/models.py
@@ -127,7 +127,7 @@ class SpamAssassinRule(models.Model):
         output = subprocess.check_output("/bin/grep -hr '^\s*describe' {0} {1} /etc/MailScanner/spam.assassin.prefs.conf /etc/mail/spamassassin /var/lib/spamassassin 2>/dev/null | /usr/bin/sort | /usr/bin/uniq".format(settings.SA_RULES_DIR, settings.SA_PREF), shell=True)
         descriptions = re.findall(r"^(?:\s*)describe\s+(\S+)\s+(.+)$", output.decode(), re.MULTILINE)
         for match in descriptions:
-            rule, created = self.objects.get_or_create(key=match[0], value=match[1])
+            rule, created = SpamAssassinRule.objects.get_or_create(key=match[0], value=match[1])
             if not created:
                 rule.value = match[1]
                 rule.save()
