6903adfa860e5f19c98e163870fa8d71e4e58b11
Kenneth Hansen <kenneth@khansen-it.dk>
2018-05-12 10:11:28 +0200

    Remove duplicate listing

--- assets/src/js/components/ListsTable.vue
+++ assets/src/js/components/ListsTable.vue
@@ -10,33 +10,14 @@
             </thead>
             <tbody>
                 <tr v-for="item in list.results" :key="item.id">
-                    <td>{{ item._from }}</td>
-                    <td>{{ item._to }}</td>
+                    <td>{{ item.from_address }}</td>
+                    <td>{{ item.to_address }}</td>
                     <td>
                         <button role="button" @click="confirmDelete(item)" class="bg-red hover:bg-red-dark text-white py-1 px-2 border border-red-light rounded shadow text-sm no-underline ml-2">Delete</button>
                     </td>
                 </tr>
             </tbody>
         </table>
-        <div class="shadow">
-            <div class="flex hover:bg-grey-lighter text-sm" v-for="item in list.results" :key="item.id">
-                <div class="text-grey-darker w-1/3 md:w-2/5 p-2">
-                    <div class="">
-                        {{ item.from_address }}
-                    </div>
-                </div>
-                <div class="text-grey-darker w-1/3 md:w-2/5 p-2">
-                    <div class="">
-                        {{ item.to_address }}
-                    </div>
-                </div>
-                <div class="text-grey-darker w-1/3 md:w-1/5 p-2">
-                    <div class="">
-                        <button role="button" @click="confirmDelete(item)" class="bg-red hover:bg-red-dark text-white py-1 px-2 border border-red-light rounded shadow text-sm no-underline ml-2">Delete</button>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="inline-flex pt-2 rounded">
             <button @click="previous" class="bg-grey-light hover:bg-grey text-grey-darkest py-2 px-4 rounded-l" :class="{'select-none cursor-not-allowed bg-grey-lightest hover:bg-grey-lightest' : list.current == 1}">
                 Prev
