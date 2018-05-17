03623e9361a94d16ca02039d080a5ccd67c4f940
Kenneth Hansen <kenneth@khansen-it.dk>
2018-05-17 19:29:13 +0200

    Add components for Material Design Icons

--- assets/src/js/components/global_components.js
+++ assets/src/js/components/global_components.js
@@ -3,7 +3,53 @@ import Modal from './Modal.vue';
 import MainWrapper from './MainWrapper.vue';
 import NotificationWrapper from './NotificationWrapper.vue';
 
+import AccountCircleIcon from '../icons/AccountCircleIcon.vue';
+import BlockIcon from '../icons/BlockIcon.vue';
+import BuildIcon from '../icons/BuildIcon.vue';
+import CheckmarkIcon from '../icons/CheckmarkIcon.vue';
+import ClearIcon from '../icons/ClearIcon.vue';
+import DashboardIcon from '../icons/DashboardIcon.vue';
+import DeleteIcon from '../icons/DeleteIcon.vue';
+import DnsIcon from '../icons/DnsIcon.vue';
+import EmailIcon from '../icons/EmailIcon.vue';
+import ExitToAppIcon from '../icons/ExitToAppIcon.vue';
+import ExpandLessIcon from '../icons/ExpandLessIcon.vue';
+import ExpandMoreIcon from '../icons/ExpandMoreIcon.vue';
+import ExposureIcon from '../icons/ExposureIcon.vue';
+import ImportContactsIcon from '../icons/ImportContactsIcon.vue';
+import InsertChartIcon from '../icons/InsertChartIcon.vue';
+import LockIcon from '../icons/LockIcon.vue';
+import MenuIcon from '../icons/MenuIcon.vue';
+import PeopleIcon from '../icons/PeopleIcon.vue';
+import PlaylistAddCheckmarkIcon from '../icons/PlaylistAddCheckmarkIcon.vue';
+import SecurityIcon from '../icons/SecurityIcon.vue';
+import SettingsIcon from '../icons/SettingsIcon.vue';
+import WidgetsIcon from '../icons/WidgetsIcon.vue';
+
 Vue.component('mw-modal', Modal);
+
+Vue.component('mw-account-circle-icon', AccountCircleIcon);
+Vue.component('mw-block-icon', BlockIcon);
+Vue.component('mw-build-icon', BuildIcon);
+Vue.component('mw-checkmark-icon', CheckmarkIcon);
+Vue.component('mw-clear-icon', ClearIcon);
+Vue.component('mw-dashboard-icon', DashboardIcon);
+Vue.component('mw-delete-icon', DeleteIcon);
+Vue.component('mw-dns-icon', DnsIcon);
+Vue.component('mw-email-icon', EmailIcon);
+Vue.component('mw-exit-to-app-icon', ExitToAppIcon);
+Vue.component('mw-expand-less-icon', ExpandLessIcon);
+Vue.component('mw-expand-more-icon', ExpandMoreIcon);
+Vue.component('mw-exposure-icon', ExposureIcon);
+Vue.component('mw-import-contacts-icon', ImportContactsIcon);
+Vue.component('mw-insert-chart-icon', InsertChartIcon);
+Vue.component('mw-lock-icon', LockIcon);
+Vue.component('mw-menu-icon', MenuIcon);
+Vue.component('mw-people-icon', PeopleIcon);
+Vue.component('mw-playlist-add-checkmark-icon', PlaylistAddCheckmarkIcon);
+Vue.component('mw-security-icon', SecurityIcon);
+Vue.component('mw-settings-icon', SettingsIcon);
+Vue.component('mw-widgets-icon', WidgetsIcon);
 
 export default {
     'mw-wrapper': MainWrapper,
---
+++ assets/src/js/icons/ExpandMoreIcon.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
+</template>
