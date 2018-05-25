e6a8e0b5d8625d6408ceea53c00a9dbd3baf4860
Kenneth Hansen <kenneth@khansen-it.dk>
2018-05-26 00:14:16 +0200

    Define base_url for premailer

--- src/mailware/settings.py
+++ src/mailware/settings.py
@@ -198,7 +198,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # Django-Premailer
-PREMAILER_OPTIONS = dict(remove_classes=True)
+BASE_URL = 'https://' + APP_HOSTNAME if SECURE_SSL_REDIRECT else 'http://' + APP_HOSTNAME
+PREMAILER_OPTIONS = dict(base_url=BASE_URL, remove_classes=True)
 
 #MailWare specific settings
 TMP_DIR = MAILWARE_ENV.get('hostconfig.tmp_dir', '/tmp')
