a70c1a5e6bb30afaa0a89b9e2e3e7168c0e51efb
Kenneth Hansen <kenneth@khansen-it.dk>
2018-05-27 03:03:10 +0200

    Remove all password hashers, so that only `Argon2` can be used

--- src/mailware/settings.py
+++ src/mailware/settings.py
@@ -144,10 +144,6 @@ OLD_PASSWORD_FIELD_ENABLED = True
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
 ]
 
 # Internationalization
