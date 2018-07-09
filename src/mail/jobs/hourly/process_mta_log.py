a4ef1238173db9d8b306eea6b190b55100b10310
Kenneth Hansen <kenneth@khansen-it.dk>
2018-07-09 20:28:32 +0200

    Fix so that we use HourlyJob instead of DailyJob

--- src/mail/jobs/hourly/process_mta_log.py
+++ src/mail/jobs/hourly/process_mta_log.py
@@ -1,10 +1,10 @@
-from django_extensions.management.jobs import DailyJob
+from django_extensions.management.jobs import HourlyJob
 from .models import Message
 from datetime import datetime, timedelta
 from django.conf import settings
 from mail.log_processors import PostfixLogProcessor
 
-class Job(DailyJob):
+class Job(HourlyJob):
     help = 'Hourly job to read the logfile of the MTA and add the log entry to mail.models.TransportLog'
 
     def execute(self):
