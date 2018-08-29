a18f2154e021c7200c888b5fdd44e032a27d95d6
Kenneth Hansen <kenneth@khansen-it.dk>
2018-08-30 01:18:26 +0200

    Add retention cleanup for tasks and notifications

---
+++ src/core/jobs/daily/clear_task_queue_retention.py
@@ -0,0 +1,13 @@
+from django_extensions.management.jobs import DailyJob
+from core.models import ApplicationTask
+from datetime import datetime, timedelta
+from django.conf import settings
+
+class Job(DailyJob):
+    help = 'Daily database maintenance to remove auditlog.models.LogEntry entries that have passed the retention policy days'
+
+    def execute(self):
+        multi_node = True if host_count > 0 else False
+        if (multi_node and not settings.API_ONLY) or not multi_node:
+            to_remove = ApplicationTask.objects.filter(completed__lt=datetime.today() - timedelta(days=settings.RECORD_RETENTION))
+            to_remove.delete()
