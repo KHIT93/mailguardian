397490b6af10a9e3e66f97ab26e7f5730a2f7252
Kenneth Hansen <kenneth@khansen-it.dk>
2018-08-29 19:54:38 +0200

    Add options for chart

--- assets/src/js/components/DashboardChart.vue
+++ assets/src/js/components/DashboardChart.vue
@@ -1,10 +1,11 @@
 <script>
 import { Line, mixins } from 'vue-chartjs'
 export default {
+    props: ['options'],
     extends: Line,
     mixins: [mixins.reactiveProp],
     created() {
-        this.renderChart(this.chartData);
+        this.renderChart(this.chartData, this.options);
     }
 }
 </script>
