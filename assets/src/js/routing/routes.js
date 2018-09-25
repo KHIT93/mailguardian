a03c0a9fae5e6832412885d1bf82a18c1212075f
Kenneth Hansen <kenneth@khansen-it.dk>
2018-09-25 19:13:26 +0200

    Add route for message contents

--- assets/src/js/routing/routes.js
+++ assets/src/js/routing/routes.js
@@ -7,6 +7,7 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Messages from '../pages/Messages/Index.vue';
 import MessageDetails from '../pages/Messages/Detail.vue';
+import MessageContents from '../pages/Messages/Contents.vue';
 import Lists from '../pages/Lists/Index.vue';
 import Reports from '../pages/Reports/Index.vue';
 import ReportMessageList from '../pages/Reports/Messages.vue';
@@ -55,6 +56,7 @@ export default [
     { path: '/login', component: Login, name: 'login', meta: { requiresAuth: false } },
     { path: '/messages', component: Messages, name: 'messages.index', meta: { requiresAuth: true } },
     { path: '/messages/:uuid', component: MessageDetails, name: 'messages.detail', props: true, meta: { requiresAuth: true } },
+    { path: '/messages/:uuid/content', component: MessageContents, name: 'messages.detail.content', props: true, meta: { requiresAuth: true } },
     { path: '/lists', component: Lists, name: 'lists.index', meta: { requiresAuth: true } },
     { path: '/reports', component: Reports, name: 'reports.index', meta: { requiresAuth: true } },
     { path: '/reports/messages', component: ReportMessageList, name: 'reports.messages', meta: { requiresAuth: true } },
