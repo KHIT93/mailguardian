418a60cca106ae444aa2cbdf7e61b5c7945f459c
Kenneth Hansen <kenneth@khansen-it.dk>
2018-09-25 19:12:43 +0200

    Remove script-tags from the html source

--- src/mail/viewsets.py
+++ src/mail/viewsets.py
@@ -73,7 +73,7 @@ class MessageViewSet(viewsets.ModelViewSet):
             if richest['content-type'].subtype == 'html':
                 data['message']['rich_version'] = richest
             elif richest['content-type'].content_type == 'multipart/related':
-                data['message']['rich_version'] = richest.get_body(preferencelist=('html')).get_content()
+                data['message']['rich_version'] = richest.get_body(preferencelist=('html')).get_content().replace('<script>', '&gt;script&lt;').replace('</scrpt>', '&gt;/script&lt;')
                 data['message']['attachments'] = []
                 for part in richest.iter_attachments():
                     data['message']['attachments'].append(part.get_filename())
