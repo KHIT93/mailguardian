184811ff7d8ebbe76c6f45bce53cbca3d0cb8585
Kenneth Hansen <kenneth@khansen-it.dk>
2020-08-05 21:40:17 +0200

    Update OS detection code

--- installer.sh
+++ installer.sh
@@ -3,52 +3,60 @@ echo 'This script will install MailGuardian, MailScanner and other required comp
 echo ''
 if ! command -v python3 &> /dev/null
 then
-    HAS_PY3 = 0
+    HAS_PY3=0
     if ! command -v python2 &> /dev/null
     then
         echo 'Unable to determine installed python version'
         exit 255
-    else
-        LNX_OS_RELEASE=$(python2 -c 'import platform; print platform.linux_distribution()[0]')
-        export $LNX_OS_RELEASE
     fi
 else
     HAS_PY3=1
-    # Python 3.8 no longer contains platform.dist() and platform.linux_distribution()
-    # A workaround has to be provided here
-    read -r -d '' PY_OS_FIND <<- EOM
-    with open('/etc/os-release', 'r') as f:\n\t
-    for l in f.readlines():\n\t\t
-        if l[:3] == 'ID=':\n\t\t\t
-            print(l.replace('ID=','').strip())\n\t\t\t
-            break
-EOM
-    LNX_OS_RELEASE=$(echo -e $PY_OS_FIND | python3)
-    if [ -z $LNX_OS_RELEASE ]
-    then
-        echo 'Unable to determine Linux version'
-        exit 255
-    fi
-    export $LNX_OS_RELEASE
 fi
-if [[ $LNX_OS_RELEASE -eq 'Ubuntu' ]]
+LNX_OS_RELEASE=$(grep '^ID=' < /etc/os-release | sed 's/ID="//g' | sed 's/"//g')
+if [ -z "$LNX_OS_RELEASE" ]
 then
-    LNX_PKG_MGR='apt'
-elif [[ $LNX_OS_RELEASE -eq 'ubuntu' ]]
-then
-    LNX_PKG_MGR='apt'
-elif [[ $LNX_OS_RELEASE -eq 'debian' ]]
-then
-    LNX_PKG_MGR='apt'
-elif [[ $LNX_OS_RELEASE -eq 'centos' ]]
-then
-    LNX_PKG_MGR='yum'
-else
-    echo 'Unable to determine Linux distribution. Only Debian, Ubuntu and CentOS are supported for this script'
+    echo 'Unable to determine Linux version'
     exit 255
 fi
-export $LNX_PKG_MGR
-read -p 'Press [Enter] to continue...'
+export LNX_OS_RELEASE
+if [ $LNX_OS_RELEASE == 'centos' ];
+then
+    echo 'Running on CentOS'
+elif [ $LNX_OS_RELEASE == 'Ubuntu' ];
+then
+    echo 'Running on Ubuntu'
+elif [ $LNX_OS_RELEASE == 'ubuntu' ];
+then
+    echo 'Running on ubuntu'
+elif [ $LNX_OS_RELEASE == 'debian' ];
+then
+    echo 'Running on debian'
+elif [ $LNX_OS_RELEASE == "pop" ];
+then
+    echo 'Running on Pop_OS'
+else
+    LNX_OS_RELEASE=$(grep '^ID=' < /etc/os-release | sed 's/ID=//g')
+    if [ $LNX_OS_RELEASE == 'centos' ];
+    then
+        echo 'Running on CentOS'
+    elif [ $LNX_OS_RELEASE == 'Ubuntu' ];
+    then
+        echo 'Running on Ubuntu'
+    elif [ $LNX_OS_RELEASE == 'ubuntu' ];
+    then
+        echo 'Running on ubuntu'
+    elif [ $LNX_OS_RELEASE == 'debian' ];
+    then
+        echo 'Running on debian'
+    elif [ $LNX_OS_RELEASE == "pop" ];
+    then
+        echo 'Running on Pop_OS'
+    else
+        echo 'Unable to determine Linux distribution. Only Debian, Ubuntu and CentOS are supported for this script'
+        exit 255
+    fi
+fi
+export LNX_PKG_MGR
 echo ''
 echo 'Installing...'
 if [[ $HAS_PY3 -eq 0 ]]
@@ -62,51 +70,52 @@ echo 'Installing git commandline tools, if not available...'
 $LNX_PKG_MGR install git -y
 echo 'Pulling application sourcecode from GitHub...'
 su - mailguardian -c 'git clone https://github.com/KHIT93/mailguardian.git /home/mailguardian/mailguardian --branch feature-new-install-scripts'
-cd /home/mailguardian/mailguardian
+cd /home/mailguardian/mailguardian || exit
 echo 'Installing required packages...'
 touch /home/mailguardian/mailguardian/installer.ini
 python3 ./installer/deps.py
 usermod -a -G mtagroup,postfix mailguardian
-if [ "$?" -ne 0 ]; then
+if ! usermod -a -G mtagroup,postfix mailguardian; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 su - mailguardian -c 'cd /home/mailguardian/mailguardian && virtualenv -p python3 . && bin/pip install -r requirements.txt'
-if [ "$?" -ne 0 ]; then
+if ! su - mailguardian -c 'cd /home/mailguardian/mailguardian && virtualenv -p python3 . && bin/pip install -r requirements.txt'; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 echo 'Installing MailGuardian...'
-export ENV_DB_PASS=$(date +%s | sha256sum | base64 | head -c 32)
+ENV_DB_PASS=$(date +%s | sha256sum | base64 | head -c 32)
+export ENV_DB_PASS
 echo 'create database mailguardian;' | su - postgres -c psql
-if [ "$?" -ne 0 ]; then
+if ! echo 'create database mailguardian;' | su - postgres -c psql; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 echo "create user mailguardian with encrypted password '$ENV_DB_PASS';" | su - postgres -c psql
-if [ "$?" -ne 0 ]; then
+if ! echo "create user mailguardian with encrypted password '$ENV_DB_PASS';" | su - postgres -c psql; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 echo 'grant all privileges on database mailguardian to mailguardian;' | su - postgres -c psql
-if [ "$?" -ne 0 ]; then
+if ! echo 'grant all privileges on database mailguardian to mailguardian;' | su - postgres -c psql; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 bin/python ./installer/mailguardian.py -f /home/mailguardian/mailguardian/installer.ini
-if [ "$?" -ne 0 ]; then
+if ! bin/python ./installer/mailguardian.py -f /home/mailguardian/mailguardian/installer.ini; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 echo 'Configure Postfix...'
 bin/python ./installer/postfix.py -f /home/mailguardian/mailguardian/installer.ini
-if [ "$?" -ne 0 ]; then
+if ! bin/python ./installer/postfix.py -f /home/mailguardian/mailguardian/installer.ini; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
 echo 'Configure MailScanner...'
 bin/python ./installer/mailscanner.py -f /home/mailguardian/mailguardian/installer.ini
-if [ "$?" -ne 0 ]; then
+if ! bin/python ./installer/mailscanner.py -f /home/mailguardian/mailguardian/installer.ini; then
     echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
     exit 1
 fi
@@ -116,9 +125,5 @@ MailScanner --lint
 
 echo 'Installation and initial configuration completed. Now we will perform some cleanup...'
 rm -rf /home/mailguardian/mailguardian/installer.ini
-if [ "$?" -ne 0 ]; then
-    echo 'We are really sorry, but something seems to have gone wrong or the script was aborted'
-    exit 1
-fi
 echo 'Installation has finished. Please reboot your system and finish the application configuration in your web browser'
 exit 0
