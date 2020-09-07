7eef774a6a2ff70f08df4635ea8caa1898ae4352
Kenneth Hansen <kenneth@khansen-it.dk>
2020-09-07 21:36:04 +0200

    Initial implementation of `compliance` app

--- src/mailguardian/settings/core_settings.py
+++ src/mailguardian/settings/core_settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'compliance.middleware.DataLogMiddleware',
 ]
 
 ROOT_URLCONF = 'mailguardian.urls'
