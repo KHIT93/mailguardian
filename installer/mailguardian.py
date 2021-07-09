c268ce6697cc3a2894a5e7ab41839ff597bfafce
Kenneth Hansen <kenneth@khansen-it.dk>
2021-07-09 08:45:42 +0200

    Remove logfile creation as it is to be handled differently

--- installer/mailguardian.py
+++ installer/mailguardian.py
@@ -240,9 +240,6 @@ if __name__ == "__main__":
         conf = conf.replace('/home/mailguardian/mailguardian', APP_DIR).replace('mailguardian', APP_USER)
         with open(os.path.join(SYSTEMD_PATH, 'mailguardian.service'), 'w') as f:
             f.write(conf)
-    os.system('mkdir -p {0}'.format(os.path.join('var', 'log', 'mailguardian')))
-    os.system('touch {0}'.format(os.path.join('var', 'log', 'mailguardian', 'app.log')))
-    os.system('chown 777 {}:{}'.format(APP_USER, 'mtagroup'))
     # Reload systemd unit cache
     os.system(SYSTEMCTL_BIN + ' daemon-reload')
 
