08e7e5c6906dfdfb74cca1aed211e1a1ef86f2db
Kenneth Hansen <kenneth@khansen-it.dk>
2021-08-10 21:07:50 +0200

    Add basic wiggle animation

--- assets/src/tailwind.config.js
+++ assets/src/tailwind.config.js
@@ -27,6 +27,15 @@ module.exports = {
       },
       fontFamily: {
         'sans': ["Roboto", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' }
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite'
       }
     },
   },
