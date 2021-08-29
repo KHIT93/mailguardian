7646160f452d9171501b55d447aa10fd18dd97f2
Kenneth Hansen <kenneth@khansen-it.dk>
2021-08-29 13:55:54 +0200

    Fix folder typo

--- assets/src/auth/index.js
+++ assets/src/auth/index.js
@@ -3,7 +3,7 @@ import { createAuth } from '@websanova/vue-auth'
 import driverHttpAxios from '@websanova/vue-auth/dist/drivers/http/axios.1.x.esm.js'
 import driverRouterVueRouter from '@websanova/vue-auth/dist/drivers/router/vue-router.2.x.esm.js'
 import router from '../router'
-import authToken from './drviers/token';
+import authToken from './drivers/token';
 
 const auth = createAuth({
     plugins: {
