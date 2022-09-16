73c2152d7b1bab392d6f301abc1b67c4145e12c7
Kenneth Hansen <kenneth@khansen-it.dk>
2022-09-16 20:51:44 +0200

    Use authentication globally

--- appui/middleware/auth.global.js
+++ appui/middleware/auth.global.js
@@ -1,5 +1,13 @@
+const GUEST_ROUTES = [
+    '/login',
+    '/password/reset'
+]
+
 export default defineNuxtRouteMiddleware((to, from) => {
     const { $auth } = useNuxtApp()
+    if (GUEST_ROUTES.includes(to.fullPath)) {
+        return
+    }
     if (!$auth().isAuthenticated()) {
         return navigateTo('/login')
     }
