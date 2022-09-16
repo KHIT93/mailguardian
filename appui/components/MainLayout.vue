d63f7b8cba456470bdf89ca61d9f0ede25f9f75c
Kenneth Hansen <kenneth@khansen-it.dk>
2022-09-16 20:55:27 +0200

    Remove usage of DialogOverlay

--- appui/components/MainLayout.vue
+++ appui/components/MainLayout.vue
@@ -3,10 +3,10 @@
         <TransitionRoot as="template" :show="open">
             <Dialog as="div" class="fixed inset-0 flex z-40 md:hidden" @close="open = false">
                 <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
-                    <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+                    <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
                 </TransitionChild>
                 <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-                    <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
+                    <DialogPanel class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
                         <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                             <div class="absolute top-0 right-0 -mr-12 pt-2">
                                 <button class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="open = false">
@@ -47,7 +47,7 @@
                                 </template>
                             </nav>
                         </div>
-                    </div>
+                    </DialogPanel>
                 </TransitionChild>
                 <div class="shrink-0 w-14" aria-hidden="true">
                     <!-- Dummy element to force sidebar to shrink to fit close icon -->
@@ -148,7 +148,7 @@
 import { onMounted, ref } from 'vue'
 import {
   Dialog,
-  DialogOverlay,
+  DialogPanel,
   Menu,
   MenuButton,
   MenuItem,
