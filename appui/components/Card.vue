63620daaab0b87f34f1c00e566d66447e4349a18
Kenneth Hansen <kenneth@khansen-it.dk>
2022-10-16 20:57:31 +0200

    Extract card component

---
+++ appui/components/Card.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-white p-4 shadow border rounded-md space-y-8 sm:space-y-5 divide-y divide-gray-200 relative pb-8">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+
+</script>
