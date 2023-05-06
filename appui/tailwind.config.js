3f63e3b383d4dcdb8d72bdf2ca97f3832d34baf0
Kenneth Hansen <kenneth@khansen-it.dk>
2023-05-06 21:02:10 +0200

    Adjust for tailwindcss 3.3

--- appui/tailwind.config.js
+++ appui/tailwind.config.js
@@ -30,7 +30,6 @@ module.exports = {
     plugins: [
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/forms'),
-        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/typography'),
     ],
 }
