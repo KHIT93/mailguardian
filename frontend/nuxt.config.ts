c88b5ea5004bf61cb98c5a18594e59b092d7bfab
Kenneth Hansen <kenneth@khansen-it.dk>
2024-12-27 22:12:27 +0100

    Migrated to Nuxt 4 app structure

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,60 +1,64 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-      enabled: true
-  },
+    future: {
+        compatibilityVersion: 4
+    },
+    
+    devtools: {
+        enabled: true
+    },
 
-  modules: [
-      '@nuxt/ui',
-    //   'nuxt-security',
-      '@nuxt-alt/auth'
-  ],
+    modules: [
+        '@nuxt/ui',
+        //   'nuxt-security',
+        '@nuxt-alt/auth'
+    ],
 
-  app: {
-      head: {
-          title: 'MailGuardian',
-          bodyAttrs: {
-              class: 'bg-slate-100'
-          }
-      }
-  },
+    app: {
+        head: {
+            title: 'MailGuardian',
+            bodyAttrs: {
+                class: 'bg-slate-100'
+            }
+        }
+    },
 
-  auth: {
-      strategies: {
-          local: {
-              url: `${process.env.SERVER_HOST}/api/v2`,
-              token: {
-                  property: 'access_token',
-                  global: true,
-                  required: true,
-                  name: 'Authorization',
-                  type: 'Bearer',
-                  httpOnly: true
-              },
-              user: {
-                  property: false,
-                  autoFetch: true
-              },
-              endpoints: {
-                  login: { url: `${process.env.SERVER_HOST}/api/v2/auth/token`, method: 'post' },
-                  logout: false,
-                  user: { url: `${process.env.SERVER_HOST}/api/v2/auth/whoami`, method: 'get' }
-              }
-          }
-      },
-      globalMiddleware: true,
-      redirect: {
-          login: '/login',
-          logout: '/login',
-          home: '/'
-      }
-  },
+    auth: {
+        strategies: {
+            local: {
+                url: `${process.env.SERVER_HOST}/api/v2`,
+                token: {
+                    property: 'access_token',
+                    global: true,
+                    required: true,
+                    name: 'Authorization',
+                    type: 'Bearer',
+                    httpOnly: true
+                },
+                user: {
+                    property: false,
+                    autoFetch: true
+                },
+                endpoints: {
+                    login: { url: `${process.env.SERVER_HOST}/api/v2/auth/token`, method: 'post' },
+                    logout: false,
+                    user: { url: `${process.env.SERVER_HOST}/api/v2/auth/whoami`, method: 'get' }
+                }
+            }
+        },
+        globalMiddleware: true,
+        redirect: {
+            login: '/login',
+            logout: '/login',
+            home: '/'
+        }
+    },
 
-  runtimeConfig: {
-    public: {
-        apiBaseUrl: `${process.env.SERVER_HOST}/api/v2`
-    }
-  },
+    runtimeConfig: {
+        public: {
+            apiBaseUrl: `${process.env.SERVER_HOST}/api/v2`
+        }
+    },
 
-  compatibilityDate: '2024-08-15'
+    compatibilityDate: '2024-08-15'
 })
